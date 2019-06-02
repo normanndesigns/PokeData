b04057be7abae554107ea8ba74235851d6b86060
Benjamin Normann <normanndesigns@outlook.com>
2019-06-02 03:17:11 +0200

    Added some basic style to the pokemon page

--- main.js
+++ main.js
@@ -5,9 +5,9 @@ const { app, BrowserWindow } = electron
 
 let win = null
 function createWindow () {
-  win = new BrowserWindow({ width: 1200, minWidth: 400, height: 600, minHeight: 200, frame: false, transparent: true, webPreferences: {nodeIntegration: true}})
+  win = new BrowserWindow({ width: 800, minWidth: 800, height: 450, minHeight: 450, frame: false, transparent: true, webPreferences: {nodeIntegration: true}})
   win.loadFile(path.join(__dirname, 'public/index.html'))
-  win.webContents.openDevTools()
+  //win.webContents.openDevTools()
   
 }
 app.on('ready', createWindow)
--- public/assets/js/pokemonscript.js
+++ public/assets/js/pokemonscript.js
@@ -1,17 +1,49 @@
 const {remote} = require('electron'); 
-var VibrantModule = require('electron').remote.require('node-vibrant')
+const fs = require('electron').remote.require('fs')
+var PokemonFile = fs.readFileSync('public/assets/data/Data.json', 'utf8')
+var PokemonData = JSON.parse(PokemonFile);
+var url = new URL(window.location.href);
+var Getpokemon = url.searchParams.get("pokemon");
+var BGcolor = url.searchParams.get("color");
 var close = document.getElementById('closeBTN').addEventListener('click', closeWindow)
 var minimize = document.getElementById('minimizeBTN').addEventListener('click', minimizeWindow)
 var maximize = document.getElementById('maximizeBTN').addEventListener('click', maximizeWindow)
+document.getElementById('name').innerHTML = Getpokemon + '<span class"DexID">' + PokemonData[Getpokemon].DexID + '</span>';
 
-var url = new URL(window.location.href);
+function hexToRgb(hex, light) {
+    var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+    r = parseInt(result[1], 16),
+    g = parseInt(result[2], 16),
+    b = parseInt(result[3], 16)
+    if(light == true){
+        return result ? {
+            r: r = r - 20,
+            g: g = g - 20,
+            b: b = b - 20
+        } : null;
+    }else{
+        return result ? {
+            r: r = r - 50,
+            g: g = g - 50,
+            b: b = b - 50
+        } : null;
+    } 
+}
 
-var Getpokemon = url.searchParams.get("pokemon");
+toolbarColor = hexToRgb('#' + BGcolor, false)
+wrapperColor = hexToRgb('#' + BGcolor, true)
 
-imgPath = __dirname + '/assets/media/' + Getpokemon.toLowerCase() + ' (2).png'
-console.log(imgPath)
-let color = new VibrantModule(imgPath)
-color.getPalette((err, palette) => console.log(palette.Vibrant.hex))
+document.getElementById('global-wrapper').style.backgroundColor = "rgb(" + wrapperColor.r + "," + wrapperColor.g + "," + wrapperColor.b + ")";
+document.getElementById('toolbar').style.backgroundColor = "rgb(" + toolbarColor.r + "," + toolbarColor.g + "," + toolbarColor.b + ")";
+
+fileExist = __dirname + "/assets/media/" + Getpokemon.toLowerCase() + ".svg";
+fs.stat(fileExist, function(err, stat) {
+    if(err == null) {
+        document.getElementById('pokemonImage').style.backgroundImage = "url(../public/assets/media/" + Getpokemon.toLowerCase() + ".svg)";
+    } else if(err.code === 'ENOENT') {
+        document.getElementById('pokemonImage').style.backgroundImage = "url('../public/assets/media/" + Getpokemon.toLowerCase() + " (2).png')";
+    }
+});
 
 window.addEventListener('load', function() {
     SpatialNavigation.init();

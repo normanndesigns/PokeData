c86c62f1df215dacf80741a0383728c0bc3446b8
Benjamin Normann <normanndesigns@outlook.com>
2019-08-10 01:17:09 +0200

    Changed the default size of the Pokedex

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ const { app, BrowserWindow } = electron
 
 let win = null
 function createWindow () {
-  win = new BrowserWindow({ width: 860, minWidth: 860, height: 650, minHeight: 450, frame: false, transparent: true, webPreferences: {nodeIntegration: true}})
+  win = new BrowserWindow({ width: 1000, minWidth: 1000, height: 700, minHeight: 700, frame: false, transparent: true, webPreferences: {nodeIntegration: true}})
   win.loadFile(path.join(__dirname, 'public/index.html'))
 }
 app.on('ready', createWindow)

c7f99cb05d0941aadbdb0ac65e513026651e6b4c
Benjamin Normann <normanndesigns@outlook.com>
2019-10-06 19:47:44 +0200

    Changed default sizes to fit better

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ const { app, BrowserWindow } = electron
 
 let win = null
 function createWindow () {
-  win = new BrowserWindow({ width: 1050, minWidth: 1050, height: 700, minHeight: 700, frame: false, transparent: true, webPreferences: {nodeIntegration: true}})
+  win = new BrowserWindow({ width: 1050, minWidth: 1050, height: 750, minHeight: 750, frame: false, transparent: true, webPreferences: {nodeIntegration: true}})
   win.loadFile(path.join(__dirname, 'public/index.html'))
 }
 app.on('ready', createWindow)

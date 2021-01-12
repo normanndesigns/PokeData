4659924605d0435b419aa9651f8325724d6ae431
Benjamin Normann <normanndesigns@outlook.com>
2021-01-12 04:49:47 +0100

    Changed the default window size

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ const { app, BrowserWindow } = electron
 
 let win = null
 function createWindow () {
-  win = new BrowserWindow({ width: 1050, minWidth: 1050, height: 700, minHeight: 700, frame: false, transparent: true, webPreferences: {nodeIntegration: true, enableRemoteModule: true}})
+  win = new BrowserWindow({ width: 1200, minWidth: 1200, height: 750, minHeight: 750, frame: false, transparent: true, webPreferences: {nodeIntegration: true, enableRemoteModule: true}})
   win.loadFile(path.join(__dirname, 'public/index.html'))
 }
 app.on('ready', createWindow)
